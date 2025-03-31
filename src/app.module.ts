9a1eea51b02625a4172463230ddcd66f9575d7d1
WiniciusNeves <winiciusneves2004@hotmail.com>
2025-03-31 17:18:03 -0300

    chore(database): setup PostgreSQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,45 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { ServicesModule } from './modules/services/services.module';
+import { ProvidersModule } from './modules/providers/providers.module';
+import { ProviderServicesModule } from './modules/ProviderServicesService/provider-services.module';
+import { FeedbacksModule } from './modules/Feedbacks/feedbacks.module';
+import { RecommendationsModule } from './modules/recommendation/recommendations.module';
+import { ReportsAbuseModule } from './modules/report-abuse/reports-abuse.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      url: 'postgresql://postgres:oIRAKrrbpnnhIlTeFOiTzCZJUfrFVLgA@metro.proxy.rlwy.net:32286/railway',
-      type: 'postgres',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
-    UserModule, // 🔹 Certifique-se de que ele está aqui
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [__dirname + '/**/*.entity.{js,ts}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    AuthModule,
+    CategoriesModule,
+    ServicesModule,
+    ProvidersModule,
+    ProviderServicesModule, 
+    FeedbacksModule,
+    RecommendationsModule,
+    ReportsAbuseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
