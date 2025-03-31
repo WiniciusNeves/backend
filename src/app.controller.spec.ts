ca23058782033d565d2987a9513e36d5eec6600b
WiniciusNeves <winiciusneves2004@hotmail.com>
2025-03-31 17:25:59 -0300

    test(global): add integration tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
-  let appService: AppService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,17 +12,14 @@ describe('AppController', () => {
     }).compile();
 
     appController = module.get<AppController>(AppController);
-    appService = module.get<AppService>(AppService);
   });
 
   it('should be defined', () => {
     expect(appController).toBeDefined();
   });
 
-  it('should return "Hello World!"', () => {
-    // Mock do método getHello do AppService
-    jest.spyOn(appService, 'getHello').mockReturnValue('Hello World!');
-
-    expect(appController.getHello()).toBe('Hello World!');
+  it('should return health check status', () => {
+    const healthCheckResponse = appController.healthCheck();
+    expect(healthCheckResponse).toEqual({ status: 'API rodando!' });
   });
 });
